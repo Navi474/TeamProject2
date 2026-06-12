d1955ae215f33baccace940ad144eea822873067
Denys <denysmlumiv@gmail.com>
2026-06-12 15:52:12 +0200

    work in progress

--- src/components/Header/SignUp/Auth.jsx
+++ src/components/Header/SignUp/Auth.jsx
@@ -1,18 +1,41 @@
 import { MdAccountCircle } from 'react-icons/md';
 import styles from './Auth.module.css';
+import { IoMdClose } from "react-icons/io";
+import {useState} from "react";
 
 export default function Auth() {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const [username, setUsername] = useState("");
+   const [modalData, setModalData] = useState({
+       username: "",
+       email: "",
+       password: "",
+   });
 
     const handleOpenModal = () => {
-        console.log("modalka");
+        setIsModalOpen(true);
     };
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setModalData({username: "", email: "", password: "", });
+    }
     return (
         <div className={styles.authContainer}>
             <button className={styles.signUpBtn} onClick={handleOpenModal}>
                 Sign Up
             </button>
             <MdAccountCircle className={styles.avatar} />
+            {isModalOpen && (
+                <div className={styles.modalOverlay}>
+                    <div className={styles.modalContent}>
+
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
