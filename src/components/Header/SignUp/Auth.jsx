524ca6c5aae371d29a8e6ff07f72806b80c82ecd
Denys <denysmlumiv@gmail.com>
2026-06-12 16:59:03 +0200

    modal and сс

--- src/components/Header/SignUp/Auth.jsx
+++ src/components/Header/SignUp/Auth.jsx
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import { MdAccountCircle } from 'react-icons/md';
 import styles from './Auth.module.css';
+import Modal from '../Modal/Modal';
 
 export default function Auth() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [userName, setUserName] = useState('');
 
     const handleOpenModal = () => {
-        console.log("modalka");
+        setIsModalOpen(true);
+    };
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+    const handleRegister = (name) => {
+        setUserName(name);
+        setIsModalOpen(false);
     };
 
     return (
         <div className={styles.authContainer}>
-            <button className={styles.signUpBtn} onClick={handleOpenModal}>
-                Sign Up
-            </button>
+            {userName ? (
+                <span
+                    style={{ fontFamily: 'var(--font-family)', fontWeight: 600, cursor: 'pointer', fontSize: '16px', color: '#000' }}
+                    onClick={() => setUserName('')}
+                    title="Logout"
+                >
+                    {userName}
+                </span>
+            ) : (
+                <button className={styles.signUpBtn} onClick={handleOpenModal}>
+                    Sign Up
+                </button>
+            )}
+
             <MdAccountCircle className={styles.avatar} />
+            {isModalOpen && (
+                <Modal onClose={handleCloseModal} onRegister={handleRegister} />
+            )}
         </div>
     );
 }
