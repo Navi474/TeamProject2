b52e66fe2e4b9bcc9903f1b1a00c327664b5fa69
Your Name <you@example.com>
2026-06-14 23:30:42 +0300

    desktop version for main dashboard 2

--- src/components/MainBlock/Dashboard/Dashboard.jsx
+++ src/components/MainBlock/Dashboard/Dashboard.jsx
@@ -4,25 +4,28 @@ import styles from './dashboard-styles/dashboardStyles.module.css';
 
 function Dashboard() {
   return (
-    <div className={styles.block}>
-      <h3 className={styles.h3}>Weather dashboard</h3>
-      <div className={styles.textColumn}>
-        <p className={styles.leftColumn}>
-          Create your personal list of favorite cities and always be aware of
-          the weather.
-        </p>
-        <div className={styles.verticalLine}></div>
-        <p className={styles.rightColumn}>
-          October 2023 <br />
-          Friday, 13<span>th</span>
-        </p>
+    <section className={styles.heroSection}>
+      <div className={styles.heroContainer}>
+        <div className={styles.mainBlock}>
+          <h3 className={styles.title}>Weather dashboard</h3>
+          <div className={styles.textColumn}>
+            <p className={styles.leftColumn}>Create your personal list of favorite cities and always be aware of the weather.</p>
+            <div className={styles.verticalLine}></div>
+            <p className={styles.rightColumn}>
+              October 2023 <br />
+              Friday, 13<span>th</span>
+            </p>
+          </div>
+          <div className={styles.searchBlock}>
+            <input className={styles.searchInput} type="text" placeholder="Search location..." />
+            <button className={styles.searchBtn}>
+              <img src="src/assets/image 2.svg" alt="search..." />
+            </button>
+          </div>
+        </div>
       </div>
-      <div className={styles.downBlock}>
-        <input className={styles.dashInput} type="text" placeholder="Search location..." /><button className={styles.searchBtn}>
-          <img src="src/assets/image 2.svg" alt="search..." />
-        </button>
-      </div>
-    </div>
+    </section>
+
   );
 }
 
